b4b89a4cd5003062fe974326a3563d860f2f3e2a
Diyorbek92 <diyorbek2017u@gmail.com>
2023-11-15 03:29:04 +0500

    mm

--- js/app.js
+++ js/app.js
@@ -7,6 +7,12 @@ const title = document.getElementById("title");
 const container = document.querySelector(".container");
 const body = document.querySelector("body");
 const musicVoice = document.getElementById("music-voice");
+const duraction = document.getElementById("duraction");
+const progress = document.getElementById("progress");
+const playIcon = document.getElementById("playIcon");
+const innerProgress = document.getElementById("inner-progress");
+const time = document.getElementById("time");
+const sec = document.getElementById("sec");
 
 body.classList.add("overlay");
 
@@ -32,27 +38,51 @@ const currentMusic = (index) => {
 
 currentMusic(currentMusicIndex);
 
-playBtn.addEventListener("click", () => {
-  if (!container.classList.contains("play")) {
-    container.classList.add("play");
-    audio.play();
-    playBtn.innerHTML = '<i class="fa-solid fa-pause"></i>';
+function pauseMusic() {
+  audio.pause();
+  container.classList.remove("play");
+  playIcon.setAttribute("class", "fa-solid fa-play");
+  container.classList.remove("play");
+}
+
+function forwardSong() {
+  if (currentMusicIndex > 0) {
+    currentMusicIndex--;
   } else {
-    audio.pause();
-    container.classList.remove("play");
-    playBtn.innerHTML = '<i class="fa-solid fa-play"></i>';
+    currentMusicIndex = musics.length - 1;
+  }
+  currentMusic(currentMusicIndex);
+  playMusic();
+}
+
+function playMusic() {
+  audio.play();
+  playIcon.setAttribute("class", "fa-solid fa-pause");
+  container.classList.add("play");
+}
+
+function uptadeProgress(e) {
+  const { duration, currentTime } = e.srcElement;
+  const proressPercent = (currentTime / duration) * 100;
+  innerProgress.style.width = `${proressPercent}%`;
+}
+
+function setProgress(e) {
+  const width = this.clientWidth;
+  const clickX = e.offsetX;
+  const duraction = audio.duration;
+}
+
+playBtn.addEventListener("click", () => {
+  if (container.classList.contains("play")) {
+    pauseMusic();
+  } else {
+    playMusic();
   }
 });
 
 forward.addEventListener("click", () => {
-  if (currentMusicIndex < musics.length - 1) {
-    currentMusicIndex++;
-  } else {
-    currentMusicIndex = 0;
-    console.log(currentMusicIndex);
-  }
-  currentMusic(currentMusicIndex);
-  audio.play();
+  forwardSong();
 });
 
 previous.addEventListener("click", () => {
@@ -68,4 +98,24 @@ previous.addEventListener("click", () => {
 
 musicVoice.addEventListener("input", (e) => {
   audio.volume = e.target.value / 100;
+});
+
+audio.addEventListener("timeupdate", uptadeProgress);
+
+audio.addEventListener("ended", (e) => {
+  forwardSong();
+});
+audio.addEventListener("timeupdate", (e) => {
+  const currentTime = e.target.currentTime;
+  const musicTime = e.target.duration;
+  const musicSec = musicTime;
+  const musicAllTime = musicSec.toFixed(2);
+  // console.log(musicAllTime / 60);
+  const sss = (currentTime / 100) * 100;
+  const www = Math.floor(sss);
+  const ddd = musicAllTime / 60 - www / 100;
+  const end = (time.innerHTML = ddd.toFixed(2));
+  const fullEnd = end.replace(".", ":").padStart(5, "0");
+  time.innerText = fullEnd;
+  sec.innerHTML = www;
 });
